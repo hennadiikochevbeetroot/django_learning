c946c25d32239b3594080dc7ab80208ea5bfdc31
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-12 12:14:12 +0300

    migrations

--- polls/models.py
+++ polls/models.py
@@ -3,5 +3,11 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(verbose_name='Question Text', max_length=100)
+    question_text = models.CharField(max_length=100)
     pub_date = models.DateTimeField(verbose_name='Date Published', auto_now=True)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(verbose_name='Choice Text', max_length=100)
+    votes = models.IntegerField(verbose_name='Amount of Votes', default=0)
