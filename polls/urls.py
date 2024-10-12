bb0b8140382aa8cfb357e626c9c1aea2df5beaae
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-12 12:59:40 +0300

    polls redirect

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.question, name='question'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('message/', views.message, name='message'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,19 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse('Hello world')
+
+
+def question(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f'Question {question_id}')
+
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f'Result for question {question_id}')
+
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f'Voted for question {question_id}')
+
+
+def message(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Message!')
