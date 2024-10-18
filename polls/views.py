5399b8f97ef111914b77a12d7f657f010c3194b6
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-18 23:34:16 +0300

    Vote fix

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.db.models import F
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls.base import reverse
 
 from .models import Choice, Question
@@ -27,22 +27,12 @@ def vote(request: HttpRequest, question_id: int) -> HttpResponse:
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(
-            request,
-            "polls/question.html",
-            {
-                "question": question,
-                "error_message": "You didn't select a choice.",
-            },
-        )
+        context = {"question": question, "error_message": "You didn't select a choice."}
+        return render(request, "polls/question.html", context)
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        return redirect(reverse("polls:results", args=(question.id,)))
 
 
 def message(request: HttpRequest) -> HttpResponse:
