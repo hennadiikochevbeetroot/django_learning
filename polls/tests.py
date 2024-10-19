7aa2e23df800039a9d17aafa183803c921d4161a
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-19 11:48:45 +0300

    Tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,19 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
 
 # Create your tests here.
+
+
+class TestQuestion(TestCase):
+    def test_was_published_recently_now(self):
+        q = Question(question_text='Question text', pub_date=timezone.now())
+        self.assertIs(q.was_published_recently(), True)
+
+    def test_was_not_published_recently_future(self):
+        q = Question(question_text='Question text', pub_date=timezone.now() + datetime.timedelta(days=7))
+        self.assertIs(q.was_published_recently(), False)
